a8cda71079cfc155595200ca93d0d32a654e20d1
GeramHov <geram.hov@gmail.com>
2022-12-13 10:25:18 +0100

    First Commit

---
+++ Squelette/js/smoothscroll.js
@@ -0,0 +1,13 @@
+const element = document.querySelector('.back-to-top');
+
+element.addEventListener('click', function (){
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+})
+
+window.addEventListener('scroll', function (){
+    if (window.scrollY === 0) {
+        element.classList.remove('active')
+    } else {
+        element.classList.add('active')
+    }
+})
